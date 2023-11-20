c21591bcaa131c7507032e41f1e94395e9e5165e
test user <test@yopmail.com>
2023-11-20 19:33:46 +0530

    Added new Classes

---
+++ src/test/java/TestCases2/TestClass2.java
@@ -0,0 +1,18 @@
+package TestCases2;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+
+	@Test
+	public void test1() {
+		System.out.println("Tested & Passed");
+
+	}
+	@Test
+	public void test2() {
+		System.out.println("Tested & Passed");
+
+	}
+
+}
